0250e041f7572975ff91adad5933c896f1bee14b
Seti-Ngabo <Seti-Ngabo@users.noreply.github.com>
2022-11-30 14:49:14 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ const config = {
   type: Phaser.Auto,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-}
+  backgroundColor: 0x5f6e7a,
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
